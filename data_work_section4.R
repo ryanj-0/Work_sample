6ace120b3187f31ced8561dc2716ee5702daeca3
rjohnson1019 <johnson.ryan1019@gmail.com>
2021-01-28 18:57:34 -0700

    seperate script to create data for plots, section4

--- data_work_section4.R
+++ data_work_section4.R
@@ -1,6 +1,6 @@
 ## Data work Section ##
-hillsdale.mspg <- yearall[IsdCode==30 & Grade==0]
-hillsdale.msgp <- hillsdale.all[!Subject=="Social Studies"]
+hillsdale.msgp <- yearall[IsdCode==30 & Grade==0]
+hillsdale.msgp <- hillsdale.msgp[!Subject=="Social Studies"]
 hillsdale.msgp <- hillsdale.msgp[!BuildingCode==0]
 
 ### Testing Group ###
@@ -31,4 +31,39 @@ hillsdale.msgp.delta <- merge(hillsdale.msgp.15,
                               suffixes = c("old","new"))
 hillsdale.msgp.delta[,delta:=MeanSGPnew-MeanSGPold]
 hillsdale.msgp.delta[,c("SchoolYearold","SchoolYearnew","MeanSGPold","MeanSGPnew"):=NULL]
-hillsdale.msgp <- hillsdale.msgp[order(DistrictCode,TestingGroup,Subject)]
+hillsdale.msgp.delta <- hillsdale.msgp.delta[order(DistrictCode, TestingGroup, Subject)]
+
+
+
+
+### Grade ###
+hillsdale.msgp.grade <- yearall[IsdCode==30 & TestingGroup=="All Students"]
+hillsdale.msgp.grade <- hillsdale.msgp.grade[!Subject=="Social Studies"]
+hillsdale.msgp.grade <- hillsdale.msgp.grade[!DistrictCode==0]
+hillsdale.msgp.grade[,c("IsdCode",
+                        "IsdName",
+                        "TestingGroup",
+                        "NumberAboveAverageGrowth",
+                        "NumberAverageGrowth",
+                        "NumberBelowAverageGrowth",
+                        "PercentAboveAverage",
+                        "PercentAverageGrowth",
+                        "PercentBelowAverage",
+                        "TotalIncluded"):=NULL] #drop columns
+hillsdale.msgp.grade.15 <- hillsdale.msgp.grade[SchoolYear=="15.16"]
+hillsdale.msgp.grade.16 <- hillsdale.msgp.grade[SchoolYear=="16.17"]
+
+# Note: Our data set is smaller. i.e. missing records across the years
+hillsdale.msgp.grade.delta <- merge(hillsdale.msgp.grade.15,
+                                    hillsdale.msgp.grade.16,
+                                    by = c("DistrictName",
+                                           "DistrictCode",
+                                           "BuildingName",
+                                           "BuildingCode",
+                                           "EntityType",
+                                           "Subject",
+                                           "Grade"),
+                                    suffixes = c("old","new"))
+hillsdale.msgp.grade.delta[,delta:=MeanSGPnew-MeanSGPold]
+hillsdale.msgp.grade.delta[,c("SchoolYearold","SchoolYearnew","MeanSGPold","MeanSGPnew"):=NULL]
+hillsdale.msgp.grade.delta <- hillsdale.msgp.grade.delta[order(DistrictCode, Grade, Subject)]
