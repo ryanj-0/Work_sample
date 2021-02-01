648630f7c9b0324bf301ed3dfe11dcbceeb88cc8
rjohnson1019 <johnson.ryan1019@gmail.com>
2021-02-01 13:50:11 -0700

    created system init file

---
+++ system_init.R
@@ -0,0 +1,29 @@
+# This code is used to scale up when directories are shared and used under same name;
+# Set working directory [setwd()] as needed
+
+# clear environment
+rm(list = ls())
+# clears graphics devices to plot later
+graphics.off()
+
+if(getwd()==paste0("C:/Users/",Sys.info()[6],"/Documents/R")){
+   dir <- getwd()
+   message(paste0("Current working directory: ", getwd()))
+} else{
+   setwd(paste0("C:/Users/",Sys.info()[6],"/Documents/R"))
+   message(paste0("Directory changed, set to: ", getwd()))
+}
+
+# Loading Needed packages
+# Checks for needed package [pacman] and then loads other packages [pkgs]
+# future development: create automated check to return missing packages using [pkgs %in% loadedNamespaces()] (01/24/2021; rzj1019)
+
+require("pacman", character.only = TRUE)
+
+pkgs <- c("data.table",
+          "tidyverse",
+          "ggthemes",
+          "ggrepel",
+          "svMisc")
+
+p_load(pkgs, character.only = TRUE)
